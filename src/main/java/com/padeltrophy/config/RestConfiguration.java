38ab9d875984d8aa4138dd924587bee2a631e79e
jalbertoribas <juan.alberto.ribas@gmail.com>
2017-03-07 10:12:24 +0100

    Enabling Cross Origin Requests

---
+++ src/main/java/com/padeltrophy/config/RestConfiguration.java
@@ -0,0 +1,28 @@
+package com.padeltrophy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Created by Juan Alberto on 21/02/2017.
+ */
+@Configuration
+public class RestConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // you USUALLY want this
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
